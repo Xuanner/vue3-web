1465ce323ca70e67758ce141fb3bfe7bb4b72cb2
xuan <13228002836@163.com>
2024-02-29 23:50:53 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+let a = 'a';
+console.log(a);
+</script>
+
+<template>
+  <div>
+    vue3 web
+    <ol v-for="i in 5">
+      <li>{{ i }}</li>
+    </ol>
+  </div>
+</template>
+
+<style scoped></style>
